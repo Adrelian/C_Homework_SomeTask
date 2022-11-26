0a72a7461c7554e7484ce649a6d56221e84736da
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-27 03:18:39 +0500

    решил задачу с доказательством треугольника

--- Program.cs
+++ Program.cs
@@ -37,27 +37,47 @@
 
 // 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
 
-void CreatArray()
-// Создаём массив из случайных и случайного размера
-{
-    //Задаём случайны размер
-    int size = new Random().Next(5, 10); 
-    int [] myArray = new int [size];
+// void CreatArray()
+// // Создаём массив из случайных и случайного размера
+// {
+//     //Задаём случайны размер
+//     int size = new Random().Next(5, 10); 
+//     int [] myArray = new int [size];
 
-    // Задаём массив
-    Console.Write("Массив -> ");
-    for (int i = 0; i < size; i++)
-    {
-        myArray[i] = new Random().Next(1, 100);
-    }
+//     // Задаём массив
+//     Console.Write("Массив -> ");
+//     for (int i = 0; i < size; i++)
+//     {
+//         myArray[i] = new Random().Next(1, 100);
+//     }
     
-    // Отображаем массив
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write($" {myArray[i] + ","}");
-    }
-    Console.WriteLine();
-}
+//     // Отображаем массив
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write($" {myArray[i] + ","}");
+//     }
+//     Console.WriteLine();
+// }
 
-void 
+
 // 41. Выяснить являются ли три числа сторонами треугольника 
+
+void inputLengthTriangleSide(int a, int b, int c)
+{
+    if ((a+b>c) && (b+c>a) && (c+a)>b) 
+    {
+        Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} существует");
+    }
+    else Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} НЕ существует");
+    }
+
+Console.Write("Введите сторону а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+inputLengthTriangleSide(a, b, c);
+
+
